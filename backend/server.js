30d5cf058805bb121c0162f5a799abe07cec8700
yan-Rab <yan.rabelo1014@gmail.com>
2020-05-05 00:50:25 -0300

    Criando aplicação React

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 mongoose.connect("mongodb://localhost:27017/systemRating", {useNewUrlParser: true});
+
 require('./src/models/Users');
 require('./src/models/Categories');
+
 app.use('/api',require('./src/routes'));
 
 app.listen(3003);
