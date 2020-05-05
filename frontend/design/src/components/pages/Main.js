2fab1e0c64018b8bd2b1003fa46c69baafcd0cf0
yan-Rab <yan.rabelo1014@gmail.com>
2020-05-05 10:50:16 -0300

    criando a página main

---
+++ frontend/design/src/components/pages/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./styles.css";
+export default class Main extends React.Component {
+    render(){
+        return(
+            <div className = 'body-main'>
+                <div className = 'body-left'>
+                    
+                </div>
+
+                <div className = 'body-right'>
+                           
+                </div>
+            </div>
+        );
+    }
+}
