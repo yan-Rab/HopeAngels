3d93359719e6372b61956a0ed0f41e0f46e78362
yan-Rab <yan.rabelo1014@gmail.com>
2020-05-06 00:50:00 -0300

    Estilizando header da page Main

--- frontend/design/src/components/pages/main/Main.js
+++ frontend/design/src/components/pages/main/Main.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
-
+import logoMarca from '../../../images/logomarca.png';
 import "./styles.css";
 export default class Main extends Component {
     render(){
         return(
             <div className = 'main-header'>
-                
-                <div>
-                    <p>Cadastre-se agora</p>
+                <img src = {logoMarca} style = {{marginTop: "10px"}}></img>
+                <div className = 'cadastro-entry'>
+                    <a href = "" className = "link-cadastro">Cadastre-se agora</a>
+                    <a href = "" className = "link-entry">Entrar</a>
                 </div>
             </div>
         )
