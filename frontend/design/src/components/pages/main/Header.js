d5d1d13c2a55d24ead69a5f427d175fcfea2d19f
yan-Rab <yan.rabelo1014@gmail.com>
2020-05-10 22:47:27 -0300

    Pequenos ajustes de responsividade na Page main

--- frontend/design/src/components/pages/main/Header.js
+++ frontend/design/src/components/pages/main/Header.js
@@ -3,7 +3,7 @@ import logoMarca from '../../../images/logomarca.png';
 import { Link } from 'react-router-dom';
 const Header = () => (
     <header className = 'main-header'>
-        <img src = {logoMarca} style = {{marginTop: "10px"}}></img>
+        <img src = {logoMarca} style = {{marginTop: "10px",marginLeft: "10%"}}></img>
         <div className = 'cadastro-entry'>
         <Link to = "/CadastroUsers" className = "link-cadastro">Cadastre-se agora</Link>
         <Link to = "/LoginUsers" className = "link-entry">Entrar</Link>
